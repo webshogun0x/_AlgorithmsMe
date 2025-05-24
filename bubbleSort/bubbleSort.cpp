6c5161439af562bc7941f7299351a5d00466a142
d5ho9un <webshogun.me@gmail.com>
2025-05-24 09:38:38 +0100

    Update: review

--- bubbleSort/bubbleSort.cpp
+++ bubbleSort/bubbleSort.cpp
@@ -5,19 +5,16 @@ void bubbleSort(std::vector<int>& toys) {
     int size = toys.size();
     bool swapped;
     
-    // Loop through all array elements
     for (int i = 0; i < size - 1; i++) {
         swapped = false;
         
         for (int j = 0; j < size - i - 1; j++) {
-            // If the element on the left is bigger than the element on the right
             if (toys[j] > toys[j + 1]) {
-                // Swap them!
                 std::swap(toys[j], toys[j + 1]);
                 swapped = true;
             }
         }
-        // If no swapping occurred in this pass, array is sorted
+        
         if (!swapped) {
             break;
         }
